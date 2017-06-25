e9c37967cce5ba837ef6b03adc605f7109dd7ffd
Riley Gowanlock <rgowanlock@gmail.com>
2017-06-24 21:45:37 -0400

    Add Deck class and Makefile

---
+++ Deck.cc
@@ -0,0 +1,49 @@
+#include "Deck.h"
+#include "Card.h"
+#include <vector>
+#include <random>
+#include <iostream>
+using std::vector;
+using std::string;
+using std::mt19937;
+
+int seed = 0;
+
+Deck::Deck() {
+    string suitList[4] = {"Club", "Diamond", "Heart", "Spade"};
+    for (int i=0; i<4; i++) {
+        for (int j=0; j<13; j++) {
+            Card::Suit suit (i);
+            Card::Rank rank (j);
+            Card* card = new Card (rank, suit);
+            cards_.push_back(card);
+        }
+    }
+    for (int i=0; i<4; i++) {
+        vector<Card*> card;
+        card.resize(13);
+        played_.push_back(card);
+    }
+};
+vector<vector<Card*>> Deck::played() {
+    return played_;
+};
+void Deck::print() {};
+void Deck::addPlayed(Card* card) {
+    int suit = card->suit().suit();
+    int rank = card->rank().rank();
+    played_[suit][rank]++;
+};
+void Deck::shuffle() {
+    static mt19937 rng(seed);
+    int n = 52;
+
+    while ( n > 1 ) {
+        int k = (int) (rng() % n);
+        --n;
+        Card *c = cards_[n];
+        cards_[n] = cards_[k];
+        cards_[k] = c;
+    } // while
+};
+
---
+++ Deck.h
@@ -0,0 +1,20 @@
+#ifndef STRAIGHTS_DECK_H
+#define STRAIGHTS_DECK_H
+
+#include "Card.h"
+#include <vector>
+using std::vector;
+
+class Deck {
+private:
+    vector<Card*> cards_;
+    vector<vector<Card*>> played_;
+public:
+    Deck();
+    vector<vector<Card*>> played();
+    void print();
+    void addPlayed(Card* card);
+    void shuffle();
+};
+
+#endif //STRAIGHTS_DECK_H
