16423baa031c825b6f57ad7dddc4f632a518ef25
Riley Gowanlock <rgowanlock@gmail.com>
2017-07-08 17:12:53 -0400

    Start button

--- GUI.cc
+++ GUI.cc
@@ -1,6 +1,6 @@
 #include "GUI.h"
 
-GUI::GUI() {
+GUI::GUI(): m_Box(Gtk::ORIENTATION_VERTICAL,5) {
     set_title("Straights");
     set_default_size(600, 400);
     set_resizable(false);
@@ -16,7 +16,13 @@ GUI::GUI() {
             m_Grid.attach(m_Card, j, i, 1, 1);
         }
     }
-    add(m_Frame);
+
+    m_Button = Gtk::Button("Start new game with seed");
+    m_Button.signal_clicked().connect(sigc::mem_fun(*this, &GUI::new_game));
+
+    add(m_Box);
+    m_Box.pack_start(m_Button);
+    m_Box.pack_start(m_Frame);
 
     m_Grid.set_column_spacing(10);
     m_Grid.set_row_spacing(5);
@@ -25,4 +31,7 @@ GUI::GUI() {
     m_Frame.set_label("Cards on the table");
 
     show_all_children();
+}
+void GUI::new_game() {
+    std::cout << "New Game" << std::endl;
 }
--- GUI.h
+++ GUI.h
@@ -8,14 +8,17 @@
 class GUI : public Gtk::Window {
 public:
     GUI();
+    void new_game();
 
 protected:
     //Child widgets:
     Gtk::Frame m_Frame;
+    Gtk::Frame m_Frame2;
     Gtk::Box m_Box;
     Gtk::Grid m_Grid;
     Gtk::Image m_Card;
     Gtk::Image* m_Card2;
+    Gtk::Button m_Button;
     std::vector<std::vector<Gtk::Image>> m_Table;
 };
 
